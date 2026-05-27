d51a3652fdfd8df9d077b47ff00c86dcb10409ab
Agustina Parache <119890962+AgusParache@users.noreply.github.com>
2026-05-27 11:48:24 -0300

    Limpieza de Dockerfile para usar Nixpacks

--- server.js
+++ server.js
@@ -23,13 +23,10 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
-        executablePath: '/usr/bin/google-chrome-stable', 
         args: [
             '--no-sandbox', 
             '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-extensions',
-            '--no-first-run'
+            '--disable-dev-shm-usage'
         ]
     }
 });
